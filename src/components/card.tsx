182ce9b74d07970a46b998adf047f8f4c6d9ba12
John Singh <johnjaskaran.singh@nvenergy.com>
2026-03-09 13:53:02 -0700

    Double click card to tap

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,13 +1,16 @@
 import { createDraggable } from "@thisbeyond/solid-dnd";
+import { createSignal } from "solid-js";
 
 export const Card = (props: { id: number; name: string }) => {
   const draggable = createDraggable(props.id);
+  const [rotated, setRotated] = createSignal(false);
   return (
     <div
       use:draggable={draggable}
-      class="cursor-grab active:cursor-grabbing select-none rounded borderx-3 py-1 shadow h-25 w-20 bg-white"
+      class="cursor-grab active:cursor-grabbing select-none rounded borderx-3 py-1 shadow h-25 w-20 bg-white transition-transform"
       classList={{ "opacity-50": draggable.isActiveDraggable }}
-      style={{ "touch-action": "none" }}
+      style={{ "touch-action": "none", transform: rotated() ? "rotate(90deg)" : "" }}
+      onDblClick={() => setRotated((r) => !r)}
     >
       <div class="flex flex-col">
         <div class="flex flex-row bg-gray-600 place-content-center text-white">
